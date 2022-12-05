25cc12b0d7a8597d2e77e4385bf538b72c3f1c08
Said <aslaxanov89@mail.ru>
2022-12-05 16:44:59 +0300

    Сделал header и category также добавил css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 // import Phones from "./components/Phones";
+import Category from "./components/categories/Category";
+import Header from "./components/header/Header";
 import PostPhone from "./components/PostPhone";
 
 function App() {
   return (
     <div className="App">
+     <Header/>
+     <Category/>
       {/* <Phones/> */}
-      <PostPhone/>
+      {/* <PostPhone/> */}
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import img from './photo/search.png'
+import img2 from './photo/basket.jpg'
+ import styles from './header.module.css'
+
+
+export default function Header() {
+  return ( 
+    
+
+
+       <header>
+    <h3>GROZNY</h3>
+    <ul>
+        <li className={styles.main}>ALL PRODUCTS</li>
+        <li className={styles.main}>SOLUTIONS</li>
+        <li className={styles.main}>ABOUT</li>
+        <li className={styles.main}>SUPPORT</li>
+    </ul>
+    
+    <div className={styles.header_end}>
+    <img className={styles.search} src={img} alt="" />
+    <img className={styles.basket} src={img2} alt="" />
+ 
+    </div>
+    
+    
+    </header> 
+    
+  )
+}
